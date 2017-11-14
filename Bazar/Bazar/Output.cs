8c1e74f5e663462840a18b24df542de14362c673
Seebzei <seebzei@gmail.com>
2017-11-14 11:12:26 +0100

    Add content to Output

--- Bazar/Bazar/Output.cs
+++ Bazar/Bazar/Output.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Bazar
 {
-	class Output
+	static class Output
 	{
+        private Output _outPut;
+        private Output() { }
+
+        /// <summary>
+        /// Returns the instance of Output
+        /// </summary>
+        /// <returns></returns>
+        public static Output getInstance()
+        {
+            if (_outPut == null)
+            {
+                _outPut = new Output();
+            }
+
+            return _outPut;
+        }
+
+        /// <summary>
+        /// Outputs given text to given outputplace yo
+        /// </summary>
+        /// <param name="text"></param>
+        public static void Write(string text)
+        {
+            if (text == null) { return; }
+            Console.WriteLine(text);
+        }
+
 	}
 }
