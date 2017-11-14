1bb0d600e466466d4d3019312b6f1f23372e145b
Seebzei <seebzei@gmail.com>
2017-11-14 11:55:26 +0100

    Init Customer

--- Bazar/Bazar/Customer.cs
+++ Bazar/Bazar/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace Bazar
 {
-	class Customer
+    class Customer
 	{
-	}
+        #region Properties
+        public int ID { get; set; }
+	    public string Name { get; set; }
+
+	    public List<Basicfood> OwnedItemsList { get; set; }
+        #endregion
+
+        #region Constructors
+	    public Customer()
+	    {
+	        ID = 0;
+	        Name = "Unnamed";
+	        OwnedItemsList = new List<Basicfood>();
+	    }
+
+	    public Customer(int id, string name)
+	    {
+	        ID = id;
+	        Name = name;
+	        OwnedItemsList = new List<Basicfood>();
+	    }
+        #endregion
+
+    }
 }
