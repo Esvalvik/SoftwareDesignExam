a1c808caad045c80a2f4efb7ea7776c48386accb
Seebzei <seebzei@gmail.com>
2017-11-15 13:26:37 +0100

    Add ItemFactory

--- Bazar/Bazar/Shop/ItemFactory.cs
+++ Bazar/Bazar/Shop/ItemFactory.cs
@@ -8,5 +8,43 @@ namespace Bazar
 {
 	class ItemFactory
 	{
-	}
+	    private ItemFactory()
+	    {
+	    }
+
+	    static IFood GetBasicFood(int id, string name, float price)
+	    {
+            return new BasicFood(id, name, price);
+	    }
+	    static IFood GetBasicFoodWithCorn(int id, string name, float price)
+	    {
+            IFood originalFood = new BasicFood(id, name, price);
+            return new DecoratorFoodCorn(originalFood);
+	    }
+
+	    static IFood GetBasicFoodWithRice(int id, string name, float price)
+	    {
+	        IFood originalFood = new BasicFood(id, name, price);
+	        return new DecoratorFoodRice(originalFood);
+	    }
+
+	    static IFood GetBasicFoodWithRawSauce(int id, string name, float price)
+	    {
+	        IFood originalFood = new BasicFood(id, name, price);
+	        return new DecoratorFoodRawSauce(originalFood);
+	    }
+
+	    static IFood GetBasicFoodWithFries(int id, string name, float price)
+	    {
+	        IFood originalFood = new BasicFood(id, name, price);
+	        return new DecoratorFoodFries(originalFood);
+	    }
+
+	    static IFood GetRandomDecoratedFood(int id)
+	    {
+            //TODO: Make random decorated food
+	        IFood originalFood = new BasicFood(id, name, price);
+	        return new DecoratorFoodCorn(originalFood);
+	    }
+    }
 }
