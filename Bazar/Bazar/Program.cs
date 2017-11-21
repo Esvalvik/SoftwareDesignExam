467e5dea44e9b8f55357e36a245b701cbe7230b1
Seebzei <seebzei@gmail.com>
2017-11-21 13:58:03 +0100

    Fixed name

--- Bazar/Bazar/Program.cs
+++ Bazar/Bazar/Program.cs
@@ -4,6 +4,14 @@ namespace Bazar
 {
     internal class Program
     {
+        public Bazar Bazar
+        {
+            get => default(Bazar.Bazar);
+            set
+            {
+            }
+        }
+
         private static void Main(string[] args)
         {
             var bazar = new Bazar();
